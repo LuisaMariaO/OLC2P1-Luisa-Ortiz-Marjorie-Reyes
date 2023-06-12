0227beff2fac973f50cb326c91ded5054fe40fb8
Luisa María Ortíz Romero <luisamaria.ortiz17@gmail.com>
2023-06-12 15:12:06 -0600

    Declaracion sin tipado

--- Fase 1/backend/src/Interpreter/Instructions/declaracion.py
+++ Fase 1/backend/src/Interpreter/Instructions/declaracion.py
@@ -17,10 +17,19 @@ class Declaracion(Instruction):
         if type(valor) == Exception:
             return
         
+        if self.tipo == None:
+            if type(valor) == int or type(valor) == float:
+                self.tipo = DataType.NUMBER
+            elif type(valor) == str:
+                self.tipo = DataType.STRING
+            elif type(valor) == bool:
+                self.tipo = DataType.BOOLEAN
+            
+       
         if self.valor.tipoDato.getTipo() != self.tipo:
-     
+  
            #Si el valor de la expresion no coincide con el de la variable, se retorna un error
-           return Exception("Semantico","El tipo de dato del valor no coincide con el de la variable",self.linea,self.columna)
+            return Exception("Semantico","El tipo de dato del valor no coincide con el de la variable",self.linea,self.columna)
         else:
           
             tablaActual = tabla
